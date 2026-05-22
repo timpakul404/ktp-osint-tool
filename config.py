b6807524516513a3b66a537a563c5caeb5240c5b
timpakul404 <hamukjangan@gmail.com>
2026-05-22 19:14:12 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,3 @@
-# ================== CONFIGURASI API KEY ==================
-
-# Ganti dengan API Key kamu dari https://api.co.id
-API_KEY = "curl -X POST https://use.api.co.id/ocr/ktp-extract \
+curl -X POST https://use.api.co.id/ocr/ktp-extract \
   -H "x-api-co-id: XdnLfGDNMYvODBs7WkukwfHhajPtatdzcQzXokwrhyguPddbGG" \
-  -F "file=@input/ktp.jpg""
-
-# =========================================================
-
-# Pengaturan lain (bisa diubah)
-API_URL = "https://use.api.co.id/ocr/ktp-extract"
-INPUT_FOLDER = "input"
-OUTPUT_FOLDER = "output"
-
-# Warna untuk tampilan di Termux
-class Colors:
-    GREEN = '\033[92m'
-    RED = '\033[91m'
-    YELLOW = '\033[93m'
-    RESET = '\033[0m'
+  -F "file=@input/ktp.jpg"
